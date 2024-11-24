784c0482e1c76090dd57ba9cd220a20ddd9c30bc
andreesantt <119643518+andreesantt@users.noreply.github.com>
2024-11-23 23:16:50 -0300

    Comentários
    Adicionados comentários ao código para uma fácil compreensão.

--- exercicios-python/ex001.py
+++ exercicios-python/ex001.py
@@ -1,7 +1,10 @@
 #Calcular média aluno
 nota1 = float(input('Primeira nota: '))
+#Aluno digita primeira nota
 nota2 = float(input('Segunda nota: '))
+#Aluno digita segunda nota
 media = (nota1 + nota2) / 2
+#Soma primeira nota com a segunda e divide por dois e encontra média
 if media >= 6:
     print('Parabéns, sua média foi {}.'.format(media))
     print('Você foi \033[4;;42mAPROVADO\033[;m')
