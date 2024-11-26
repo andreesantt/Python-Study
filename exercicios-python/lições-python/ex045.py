99090822a5f51e24e2aed289b7dd77f4b852b1d5
andreesantt <119643518+andreesantt@users.noreply.github.com>
2024-11-26 09:24:07 -0300

    Melhorias nas estruturas condicionais
    ° Melhorias nas mensagens de vencedor.
    ° Corrigido condições redundantes.
    ° Corrigido prints inválidos.

--- exercicios-python/lições-python/ex045.py
+++ exercicios-python/lições-python/ex045.py
@@ -1,7 +1,7 @@
 from random import randint
 itens = ('Pedra', 'Papel', 'Tesoura') #Itens a serem escolhidos
 computer = randint(0, 2) #Ecolha do computador
-print('=-'*15)
+print('=-'*15)  
 print('Vamos jogar Jokempô ? ')
 print('''Qual sua escolha:
 [ 00 ] PEDRA
@@ -9,28 +9,21 @@ print('''Qual sua escolha:
 [ 02 ] TESOURA''')
 player = int(input('Faça sua escolha: '))
 from time import sleep
+# Agguarda mostrar resultados
 print('AGUARDE...')
 sleep(3)
-#Situações onde computador e jogador escolhem os mesmos itens.
-if computer == 0 and player == 0:
+# Exibe escolhas
+print(f'Você escolheu {itens[player]}')
+print(f'Eu escolhi {itens[computer]}')
+# Situações onde computador e jogador escolhem os mesmos itens.
+if player == computer:
     print('EMPATE')
-elif computer == 1 and player == 1:
-        print('EMPATE')
-elif computer == 2 and player == 2:
-            print('EMPATE')
-#Situações computador ganha.
-if computer == 0 and player == 2:
-    print('Que pena pra você! Eu escolhi {}. Eu venci '.format(itens[computer]))
-elif computer == 1 and player == 0:
-    print('Que pena pra você! Eu escolhi {}. Eu venci '.format(itens[computer]))
-elif computer == 2 and player == 1:
-    print('Que pena pra você! Eu escolhi {}. Eu venci '.format(itens[computer]))
+#Situações computador vence.
+elif (computer == 0 and player == 2) or (computer == 1 and player == 0) or (computer == 2 and player == 1):
+    print('Que pena pra você. Eu venci')
 #Situações onde jogador vence
-if player == 0 and computer == 2:
-    print('Que sorte! Eu escolhi {}. Você venceu'.format(itens[computer]))
-elif player == 1 and computer == 0:
-    print('Que sorte! Eu escolhi {}. Você venceu'.format(itens[computer]))
-elif player == 2 and computer == 1:
-    print('Que sorte! Eu escolhi {}. Você venceu'.format(itens[computer]))
+elif (player == 0 and computer == 2) or (player == 1 and computer == 0) or (player == 2 and computer == 1):
+    print('Que sorte! Você venceu')
+# Finaliza o jogo
 sleep(3)
 print('=-'*15)
