43cd75a248375fbf08220b832c6be6630ed6ddbb
andreesantt <119643518+andreesantt@users.noreply.github.com>
2024-11-27 15:44:53 -0300

    updates triangulo

--- exercicios-python/lições-python/ex042.py
+++ exercicios-python/lições-python/ex042.py
@@ -1,14 +1,14 @@
 #Programa que lê três comprimentos e mostra se forma um triangulo, se sim qual triângulo.
-t1 = int(input('Comprimento 01: '))
-t2 = int(input('Comprimento 02: '))
-t3 = int(input('Comprimento 03: '))
-if t1 + t2 > t3 and t1 + t3 > t2 and t2 + t3 > t1:
-    print('Paranbéns você conseguiu formar um triangulo ')
-if t1 == t2 and t1 == t3 and t2 == t2:
-    print('O triangulo é hequilatéro')
-elif t1 == t2 != t3 or t1 == t3 != t2:
-    print('O triângulo é Isóceles')
-elif t1 != t2 and t1 != t3 or t2 != t3:
-    print('O triângulo é Escaleno')
+comprimento1 = int(input('Digite o primeiro comprimento: '))
+comprimento2 = int(input('Digite o segundo comprimento: '))
+comprimento3 = int(input('Digite o terceiro comprimento: '))
+if comprimento1 + comprimento2 > comprimento3 and comprimento1 + comprimento3 > comprimento2 and comprimento2 + comprimento3 > 1:
+    print('Parabéns, você formou um triângulo')
 else:
-    print('Você não conseguiu formar um triângulo')
+    print('Você não pode formar um triângulo com esses dados')
+if comprimento1 == comprimento2 == comprimento3:
+    print('Este triangulo é Equilátero')
+elif comprimento1 == comprimento2 != comprimento3 or comprimento1 == comprimento3 != comprimento2 or comprimento2 == comprimento3 != 1:
+    print('O triangulo é isóceles')
+else:
+    print('O triangulo é Escaleno')
