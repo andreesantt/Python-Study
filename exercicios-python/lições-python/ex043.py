c1da53306d32bba53728556c06224355333e8ac0
andreesantt <119643518+andreesantt@users.noreply.github.com>
2024-11-27 14:52:42 -0300

    Código funcional
    Modificações na estrutura condicional  cálculo de índice de Massa Corporal do Individuo.

--- exercicios-python/lições-python/ex043.py
+++ exercicios-python/lições-python/ex043.py
@@ -1,13 +1,19 @@
 #Leia peso e altura usuário e calcule IMC e mostre sua situação atual
-peso = float(input('Digite seu peso: '))
-altura = float(input('Digite sua altura: '))
-imc = peso // (altura ** 2)
-if 18.5 <= imc:
-    print('seu IMC equivale a {}\nSegundo a Organização Mundial de Saúde\nVocê está \033[0;;41mABAIXO\033[;m do peso ideal'.format(imc))
-elif imc >= 18.5 or imc <= 25:
-    print('Seu IMC resuta em {}\nVocê está no seu peso \033[4;0;42mIDEAL\033[;m'.format(imc))
-elif imc >= 25 or imc <= 30:
-    print('Alerta de sáude\nSeu IMC está em {}\nVocê está com \033[4;0;46mSOBREPESO\033[;m'.format_map(imc))
+peso_usuario = float(input('Digite seu peso: '))
+altura_usuario = float(input('Digite sua altura: '))
+alt2 = altura_usuario ** 2
+imc = peso_usuario / alt2
+print(f'Seu índice de massa corporal é {imc:.2f}, de acordo com os dados fornecidos\nE cálculo do seu IMC vamos identificar sua situação atual')
+from time import sleep
+print('AGUARDE...')
+sleep(3)
+if imc < 18.5:
+    print('Você está abaixo do peso')
+elif 18.5 <= imc < 25:
+    print('Você está no seu peso ideal')
+elif 25 <= imc < 30:
+    print('Você está em sobrepeso, precisa se cuidar')
+elif 30 <= imc < 40:
+    print('Obesidade, precisa cuidar da saúde')
 else:
-    30 <= imc or 40 <= imc:
-        print('Alerta de saúde\nIMC em {}\nVocê está com \033[;;41mOBESIDADE\033;m'.format(imc))
+    print('Você está em obesidade mórbida, precisa urgente se cuidar')
